f1f14a76523b033240ed82e210001027b876e3c7
Donny Vu <donnyvu@donnys-mbp.lan>
2021-01-14 23:23:59 -0600

    adding age and position to player card

--- src/App.js
+++ src/App.js
@@ -2,35 +2,33 @@ import './App.css';
 import { PlayerCard } from './player-card'
 // import PlayerCard from './player-card'
 const players = [
-  { name: "Stephen Curry", team: "Golden State Warriors", age: 32, img: "steph-curry.jpg" },
-  { name: "Kyrie Irving", team: "Brooklyn Nets", age: 28, img: "kyrie-irving.jpg" },
-  { name: "Jayson Tatum", team: "Boston Celtics", age: 22, img: "jayson-tatum.jpg" },
-  { name: "Devin Booker", team: "Phoenix Suns", age: 24, img: "devin-booker.jpeg" }
+  { name: "Stephen Curry", team: "Golden State Warriors", age: 32, position: 'PG', img: "steph-curry.jpg" },
+  { name: "Kyrie Irving", team: "Brooklyn Nets", age: 28, position: 'PG', img: "kyrie-irving.jpg" },
+  { name: "Jayson Tatum", team: "Boston Celtics", age: 22, position: 'SF', img: "jayson-tatum.jpg" },
+  { name: "Devin Booker", team: "Phoenix Suns", age: 24, position: 'SG', img: "devin-booker.jpeg" }
 ];
 
-function App() {
+const App = () => {
   return (
     <div className="container">
       <div className="row">
         {
           players.map((player) => {
             return <div className="col-3">
-              <PlayerCard name={player.name} team={player.team} img={player.img} ></PlayerCard>
+              <PlayerCard name={player.name} team={player.team} position={player.position} age={player.age} img={player.img} ></PlayerCard>
             </div>
           })
         }
         {
           players.filter((player) => player.age < 25).map(player => {
             return <div className="col-6">
-              <PlayerCard name={player.name} team={player.team} img={player.img}></PlayerCard>
+              <PlayerCard name={player.name} team={player.team} position={player.position} age={player.age} img={player.img}></PlayerCard>
             </div>
           })
         }
       </div>
     </div>
-
   );
 }
-// make a .filter for players under age of 25
 // make a .find for specific player
 export default App;
