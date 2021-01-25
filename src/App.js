b1c88775d7b8488951589cb0794418a2d687ef22
Donny Vu <donnyvu@donnys-mbp.lan>
2021-01-25 17:24:11 -0600

    dropdown-filter feature completed

--- src/App.js
+++ src/App.js
@@ -10,29 +10,83 @@ const players = [
 ];
 
 const App = () => {
+
   const [currentPlayers, setCurrentPlayers] = useState(players);
-  const handleChange = (event) => {
-    const searchedPlayer = event.target.value.toLowerCase();
-    console.log(searchedPlayer);
+  const [currentCategory, setCurrentCategory] = useState(null);
+  const [err, setErr] = useState(null);
+  const [currentSearchInputValue, setCurrentSearchInputValue] = useState(null);
+
+  const handleFilterPlayerInputSearch = (inputFieldValue) => {
+    console.log('input value:', inputFieldValue)
+    if (!currentCategory && inputFieldValue) {
+      setErr('Please Select a Category.')
+      return;
+    }
+    if (currentCategory && !inputFieldValue) {
+      setErr('')
+    }
+    if (!inputFieldValue) {
+      setCurrentPlayers(players);
+      return;
+    }
+    console.log('current category:', currentCategory);
+
+    if(currentCategory && inputFieldValue){
+      setCurrentSearchInputValue(inputFieldValue)
+      setCurrentPlayers(filteredPlayers)
+      return;
+    }
+
     const filteredPlayers = players.filter((player) => {
-      console.log(player.name.toLowerCase());
-      return player.name.toLowerCase().includes(searchedPlayer)
-    });
-    setCurrentPlayers(filteredPlayers)
-  }
+      console.log(player[currentCategory]);
+      return player[currentCategory].toString().includes(inputFieldValue)
+    })
+    setCurrentPlayers(filteredPlayers);
+    setCurrentSearchInputValue(inputFieldValue);
+  };
+
+  const handleCategorySelection = (category) => {
+    console.log(category);
+    if (category) {
+      setErr('')
+    }
+    setCurrentCategory(category);
+    if(!currentSearchInputValue){
+      setCurrentPlayers(players);
+      return;
+    }
+  //   const filteredPlayers = players.filter((player) => {
+  //     console.log(player[category]);
+  //     return player[category].toString().includes(currentSearchInputValue)
+  //   })
+  //   setCurrentPlayers(filteredPlayers);
+  } 
+
+  // const handleChange = (event) => {
+  //   const searchedPlayer = event.target.value.toLowerCase();
+  //   console.log(searchedPlayer);
+  //   const filteredPlayers = players.filter((player) => {
+  //     console.log(player.name.toLowerCase());
+  //     return player.name.toLowerCase().includes(searchedPlayer)
+  //   });
+  //   setCurrentPlayers(filteredPlayers)
+  // }
 
   return (
     <div className="container">
       <div className="row">
-        <div class="input-group mb-3">
-          <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Search Player</button>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Player Name</a></li>
-            <li><a class="dropdown-item" href="#">Team</a></li>
-            <li><a class="dropdown-item" href="#">Position</a></li>
-            <li><a class="dropdown-item" href="#">Age</a></li>
-          </ul>
-          <input type="text" class="form-control" aria-label="Text input with dropdown button" />
+        <div><h3> Current Category: {currentCategory}</h3></div>
+        <div><h3>Current Input Value: {currentSearchInputValue}</h3></div>
+        <div><h3>{err}</h3></div>
+        <div className="input-group mb-3">
+          <select class="form-select" aria-label="Default select example" onChange={(event) => handleCategorySelection(event.target.value)}>
+            <option selected value="">Select Category:</option>
+            <option value="name">Name</option>
+            <option value="team">Team</option>
+            <option value="position">Position</option>
+            <option value="age">Age</option>
+          </select>
+          <input type="text" class="form-control" aria-label="Text input with select option" onChange={(event) => handleFilterPlayerInputSearch(event.target.value)} />
         </div>
         {
           currentPlayers.map((player) => {
@@ -47,9 +101,11 @@ const App = () => {
             </div>
           })
         }
+
       </div>
     </div>
   );
 }
 // make a dropdown that shows name team and position, whatever the drop down is, is what i want to filter find in the search bar.
+// on the dropdown, click the option that you want to filter by (name, team, position, age)
 export default App;
